ba5c537ab1533ea2f102ac52301b6386c4cfe330
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-11-01 21:40:54 +0100

    Things get more complicated in the demo

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>Hello Vite, pong via socket.io => {{ pong }}</div>
   <div>
-    <iframe src="/zerva"></iframe>
-    <iframe src="/data.json"></iframe>
+    <h1>zerva-websocket demo</h1>
+    <p>Response of viteEcho request: <pre>{{ directFeedback }}</pre></p>
+    <p>Pushed viteEcho: <pre>{{ pushedFeedback }}</pre></p>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue"
+<script setup lang="ts">
+import { ref } from "vue"
 import { Logger, useMessageHub } from "zeed"
 import { WebSocketConnection } from "zerva-websocket"
 import { Messages } from "./protocol"
@@ -18,24 +18,26 @@ log("app")
 const channel = new WebSocketConnection()
 const msg = useMessageHub({ channel }).send<Messages>()
 
-export default defineComponent({
-  setup() {
-    let pong = ref("")
+let directFeedback = ref({})
+let pushedFeedback = ref({})
 
-    // conn.on("serverPong", (data) => log("serverPong", data))
+// conn.on("serverPong", (data) => log("serverPong", data))
 
-    // conn.emit("serverPing", { echo: uuid() }).then((r: any) => {
-    //   log("pong", r)
-    //   pong.value = r
-    // })
+// conn.emit("serverPing", { echo: uuid() }).then((r: any) => {
+//   log("pong", r)
+//   pong.value = r
+// })
 
-    msg
-      .viteEcho({ hello: "world" })
-      .then((data) => log("viteEcho direct", data))
-
-    return {
-      pong,
-    }
+useMessageHub({ channel }).listen<Messages>({
+  viteEcho(data) {
+    log("received", data)
+    pushedFeedback.value = data 
+    return data
   },
+})
+
+msg.viteEcho({ hello: "world at " + new Date() }).then((data: any) => {
+  log("viteEcho direct", data)
+  directFeedback.value = data
 })
 </script>
